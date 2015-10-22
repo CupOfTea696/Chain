045d3c12383aa13b3ee1278fccb9b7458a369120
Sven Wittevrongel <cupoftea696@gmail.com>
2015-10-22 10:33:13 -0400

    Applied fixes from StyleCI

--- src/Exceptions/InvalidMethodException.php
+++ src/Exceptions/InvalidMethodException.php
@@ -4,12 +4,10 @@ use Exception;
 
 class InvalidMethodException extends Exception
 {
-    
     public function __construct($class, $method, $code = 0, Exception $previous = null)
     {
         $message = "The method $class::$method() does not exist.";
         
         parent::__construct($message, $code, $previous);
     }
-    
 }
--- src/Exceptions/WrongClassException.php
+++ src/Exceptions/WrongClassException.php
@@ -4,12 +4,10 @@ use Exception;
 
 class WrongClassException extends Exception
 {
-    
     public function __construct($class, $isnt, $code = 0, Exception $previous = null)
     {
-        $message = "The class $class is not an instance of $isnt as required by " . Chain::class . "::requires().";
+        $message = "The class $class is not an instance of $isnt as required by " . Chain::class . '::requires().';
         
         parent::__construct($message, $code, $previous);
     }
-    
 }
